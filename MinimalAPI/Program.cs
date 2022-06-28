e2b8449db7fe5d12779d005d2d9d27b987ec47d8
danhdeng <hui_danny_deng@hotmail.com>
2022-06-28 00:15:39 -0400

    added create command, update command, delete command and get command by id api in program file

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Data;
+using MinimalAPI.Dtos;
 using MinimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,7 +33,46 @@ app.UseHttpsRedirection();
 
 app.MapGet("api/v1/commands", async (ICommandRepo repo, IMapper mapper)=>{
     var commands=await repo.GetAllCommands();
-    return Results.Ok(mapper.Map<IEnumerable<Command>>(commands));
+    return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+});
+
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, [FromRoute]int id)=>{
+    var command=await repo.GetCommandById(id);
+    return Results.Ok(mapper.Map<CommandReadDto>(command));
+});
+
+app.MapPost("api/v1/commands", async (ICommandRepo repo, IMapper mapper, CommandCreateDto commandCreateDto)=>{
+    var commandModel=mapper.Map<Command>(commandCreateDto);
+    await repo.CreateCommand(commandModel);
+    await repo.SaveChanges();
+    var commandReadDto=mapper.Map<CommandReadDto>(commandModel);
+    return Results.Created($"api/v1/commands/{commandReadDto.Id}", commandReadDto);
+});
+
+app.MapPut("api/v1/commands", async (ICommandRepo repo, IMapper mapper, int id, CommandUpdateDto commandUpdateDto)=>{
+    var command=await repo.GetCommandById(id);
+    if (command == null)
+    {
+        return Results.NotFound();
+    }
+    mapper.Map(commandUpdateDto,command);
+    await repo.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapDelete("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) => {
+    var command = await repo.GetCommandById(id);
+    if (command == null)
+    {
+        return Results.NotFound();
+    }
+
+    repo.DeleteCommand(command);
+
+    await repo.SaveChanges();
+
+    return Results.NoContent();
+
 });
 
 app.Run();
